58d6e4cda509b5535579def857dbdefd0a95b9c5
Mark Cheverton <mark.cheverton@red-gate.com>
2012-02-14 17:10:17 +0000

    Moved bot tracking into class var. Support for hostnames in IRC speak calls.

--- lib/qswarm/speakers/irc.rb
+++ lib/qswarm/speakers/irc.rb
@@ -2,43 +2,43 @@ require 'cinch'
 
 module Qswarm
   module Speakers
-    class IRC < Qswarm::Speaker
-      dsl_accessor :irc_server
-
+    class Irc < Qswarm::Speaker
+      @@irc_servers = {}
+        
       def inject(format = :text, msg)
         logger.debug "[#{@agent.name}] Sending '#{msg}' to channel #{@name}"
         publish format, @name, msg
       end
 
       def run
-        logger.debug "Connecting to IRC server #{@irc_server ||= 'localhost'} channel #{@name}"
-        
-        # This needs fixing. Global vars oh dear
-        if $bot
-          $bot.config.channels << @name
-        else
-          $bot = Cinch::Bot.new
-          # @bot.config['server'] = @irc_server ||= 'localhost'
-          # @bot.config['channels'] = @name.split
-          # $bot.config.server = @irc_server ||= 'localhost'
-          $bot.config.channels << @name
-          $bot.config.nick = @agent.name
-
-          EM.defer do
-            $bot.start
-          end
-        end
+        @name.match(/([^#]*)(#.+)/) { irc_connect $1.empty? ? 'localhost' : $1, $2 }
       end
       
       private
+      
+      def irc_connect(irc_server, channel)
+        if bot = @@irc_servers[irc_server]
+          logger.debug "Joining channel #{channel}"
+          @@irc_servers[irc_server].config.channels << channel
+        else
+          logger.debug "Connecting to IRC server #{irc_server} channel #{channel}"
+          @@irc_servers[irc_server]               = Cinch::Bot.new
+          @@irc_servers[irc_server].config.server = irc_server
+          @@irc_servers[irc_server].config.nick   = @agent.name
+          @@irc_servers[irc_server].config.channels << channel
+          
+          EM.defer do
+            @@irc_servers[irc_server].start
+          end
+        end
+      end
 
-      def publish(format, channel, msg)
-        case format
-        when :json
-          $bot.channel_manager.find_ensured(channel).andand.send(JSON.generate(msg))
-        when :text
-          $bot.channel_manager.find_ensured(channel).andand.send(msg)
-        end    
+      def publish(format, name, msg)
+        @name.match(/([^#]*)(#.+)/) do
+          @@irc_servers[$1.empty? ? 'localhost' : $1].channel_manager.find_ensured($2).andand.send(
+            format == :json ? JSON.generate(msg) : msg
+          )
+        end  
       end
     end
   end
