af91109f1b38cbe36f1cdcdb2376c07770c56051
Mark Cheverton <mark.cheverton@ecafe.org>
2012-03-12 14:12:04 +0000

    Spelling errors!

--- lib/qswarm/loggable.rb
+++ lib/qswarm/loggable.rb
@@ -9,7 +9,7 @@ module Qswarm
 
     def self.logger
       # @logger ||= Logger.new(STDOUT)
-      @logger ||= GELF::Logger.new($graylog2_host, 12201, 'WAN', { :facility => $greylog2_facility })
+      @logger ||= GELF::Logger.new($graylog2_host, 12201, 'WAN', { :facility => $graylog2_facility })
     end
   end
 end
