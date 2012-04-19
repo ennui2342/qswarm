3db17e9a3c3c0cd6805923ddf2ccdf5c5196b932
Mark Cheverton <mark.cheverton@ecafe.org>
2012-04-19 16:01:04 +0100

    Added hooks to process incoming IRC events

--- lib/qswarm/speakers/irc.rb
+++ lib/qswarm/speakers/irc.rb
@@ -1,10 +1,16 @@
 require 'cinch'
+require 'ostruct'
 
 module Qswarm
   module Speakers
     class Irc < Qswarm::Speaker
       @@irc_servers = {}
-        
+
+      def initialize(listener, name, args, &block)        
+        @admin_host = args[:admin_host]
+        super
+      end
+      
       def inject(format = :text, msg)
         logger.debug "[#{@agent.name}] Sending '#{msg}' to channel #{@name}"
         publish format, @name, msg
@@ -13,7 +19,20 @@ module Qswarm
       def run
         @name.match(/([^#]*)(#.+)/) { irc_connect $1.empty? ? 'localhost' : $1, $2 }
       end
-      
+            
+      def parse(m)
+       logger.debug "Received message: #{m.channel}/#{m.message}"
+       super( OpenStruct.new( :routing_key => '__', :message => m, :channel => m.channel ), m.message )
+      end
+
+      def join(m)
+        unless m.user.nick == m.bot.nick
+          if @admin_host && Regexp.new(@admin_host).match(m.user.host)
+            m.channel.op(m.user)
+          end
+        end
+      end
+
       private
       
       def irc_connect(irc_server, channel)
@@ -22,10 +41,25 @@ module Qswarm
           @@irc_servers[irc_server].config.channels << channel
         else
           logger.debug "Connecting to IRC server #{irc_server} channel #{channel}"
-          @@irc_servers[irc_server]               = Cinch::Bot.new
+          
+          @@irc_servers[irc_server]               = Cinch::Bot.new do
+            on :channel do |m|
+              if m.message =~ /^#{m.bot.nick}/
+                EM.defer do
+                  m.bot.config.shared['speaker'].parse(m)
+                end
+              end
+            end
+            
+            on :join do |m|
+              m.bot.config.shared['speaker'].join(m)
+            end
+          end
+          
           @@irc_servers[irc_server].config.server = irc_server
           @@irc_servers[irc_server].config.nick   = @agent.name
           @@irc_servers[irc_server].config.channels << channel
+          @@irc_servers[irc_server].config.shared['speaker'] = self
           
           EM.defer do
             @@irc_servers[irc_server].start
@@ -35,11 +69,12 @@ module Qswarm
 
       def publish(format, name, msg)
         @name.match(/([^#]*)(#.+)/) do
-          @@irc_servers[$1.empty? ? 'localhost' : $1].channel_manager.find_ensured($2).andand.send(
+          @@irc_servers[$1.empty? ? 'localhost' : $1].channel_list.find_ensured($2).andand.send(
             format == :json ? JSON.generate(msg) : msg
           )
         end  
       end
+        
     end
   end
 end
